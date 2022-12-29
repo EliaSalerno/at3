b31865a133f1794812c09e1eb5bdb506c7c3a53f
elia.salerno <elia.salerno@issgreppi.it>
2022-12-29 16:43:23 +0100

    Modifica file di creazione utenze

--- us.sh
+++ us.sh
@@ -1,9 +1,11 @@
+a=0
 for i in $(cat utenza);
 do
 	ut=${i%-*}
 	ps=${i#*-}
 	useradd -m "$ut@issgreppi.it"
 	echo -e "$ut@issgreppi.it:$ps" | chpasswd
+	a=$((a+1))
 done
-ls -l /home
 chmod 750 /home/*
+echo "Utenti creati: $a"
