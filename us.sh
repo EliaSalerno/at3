225c2bff5c8908a0dc522d7cf6a0b1ceae07271b
EliaSalerno <elia.salerno@issgreppi.it>
2023-01-16 10:49:39 +0000

    Master: modifica file creazione utenze

--- us.sh
+++ us.sh
@@ -1,11 +1,18 @@
 a=0
-for i in $(cat utenza);
+for j in $(ls /utenza);
 do
-	ut=${i%-*}
-	ps=${i#*-}
-	useradd -m "$ut@issgreppi.it"
-	echo -e "$ut@issgreppi.it:$ps" | chpasswd
-	a=$((a+1))
+	for i in $(cat utenza/$j);
+	do
+		ut=${i%-*}
+		ps=${i#*-}
+		useradd -m "$ut@issgreppi.it"
+		echo -e "$ut@issgreppi.it:$ps" | chpasswd
+		a=$((a+1))
+		touch "/home/.confcl"
+		echo "  Classe: $j">>"/home/.confcl"
+		echo "  Utente: $ut">>"/home/.confcl"
+		chmod 500 "/home/.confcl"
+	done
 done
 chmod 750 /home/*
 echo "Utenti creati: $a"
